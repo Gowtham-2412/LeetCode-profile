d932968bb0b0d1d6fc638ddb6824120efe4c449d
Gowtham Konijeti <sgowtham.konijeti@gmail.com>
2026-06-27 23:06:02 +0530

    Time: 1061 ms (5.04%), Space: 60.6 MB (5.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,7 +3,6 @@ public:
     bool checkInclusion(string s1, string s2) {
         if(s1.size() > s2.size()) return false;
         int left = 0;
-        bool consist = false;
         int n = s1.size();
         sort(s1.begin(), s1.end());
         int right = left+n-1;
